0504009a9566974fc0f3fd986f98701ce4bb5daa
Kavindu-Kanchana <mmkavindukanchana@gmail.com>
2024-06-02 09:55:57 +0530

    Create Footer.js

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="relative">
+      <footer className="bg-green-500 py-4 text-center w-full">
+        <div className="container mx-auto">
+          <p className="text-white">&copy; Ayurvedic Spa - 2023</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
